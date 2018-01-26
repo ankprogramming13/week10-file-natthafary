d9c867666b32eef0b039c9ad7de3a6baf163d734
fary <farynanmonot@gmail.com>
2018-01-26 13:41:36 +0700

    ข้อ1,2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
 int main() {
         /* code */
+        string line;
+        ofstream myfile ("example.txt");
+        if (myfile.is_open())
+        {
+            myfile ( getline(myfile,line) )
+            {
+                cout << line << '\n';
+            }
+            myfile.close();
+        }
+        else cout << "Unable to open file";
         return 0;
 }
